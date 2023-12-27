909fa537398e7edd5fbf8ba640c7aba7b56177e6
Lucky LukeHjo <lukehjo@gmail.com>
2023-12-27 03:39:38 +0100

    1.0.4

--- configs/pwsh/Microsoft.PowerShell_profile.ps1
+++ configs/pwsh/Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,7 @@
 
 # Author: LukeHjo (Azrael)
 # Description: This is my PowerShell profile. It contains aliases and functions that I use on a daily basis.
-# Version: 1.0.3
+# Version: 1.0.4
 # Date: 2023-12-27
 
 # ----------------------------------------
