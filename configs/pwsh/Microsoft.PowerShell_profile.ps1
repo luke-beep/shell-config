b85a987e484eec8de0d44b01836ea0200e3970cb
Azrael <lukehjo@duck.com>
2024-08-26 22:28:00 +0200

    version: 1.3.3 -> 1.3.4.

--- configs/pwsh/Microsoft.PowerShell_profile.ps1
+++ configs/pwsh/Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,7 @@
 
 # Author: LukeHjo (Azrael)
 # Description: This is my PowerShell profile. It contains features that I use on a daily basis.
-# Version: 1.3.3
+# Version: 1.3.4
 # Date: 2024-01-12
 
 # ----------------------------------------
